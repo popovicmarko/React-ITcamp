be84fd8babc3cdd3cde65c22322186af1ded9a54
popovicmarko <popovicmarko350@gmail.com>
2023-05-02 18:58:18 +0200

    css register problems heigth

--- project-nit/src/components/register/Register.js
+++ project-nit/src/components/register/Register.js
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import './Register.css'
 export function Register() {
+    function handleClick(e) {
+
+    }
+    const [username, setUsername] = useState('')
     return (
         <div className='cointener'>
             <form>
                 <h1>Register</h1>
                 <label>Name</label>
-                <input type="text" placeholder="First Name" name="firstname" required></input>
+                <input className="rInput" type="text" placeholder="First Name" name="firstname" required></input>
                 <label>Surame</label>
-                <input type="text" placeholder="Last Name" name="lasttname" required></input>
+                <input  type="text" className="rInput" placeholder="Last Name" name="lasttname" required></input>
                 <label>Username</label>
-                <input type="text" placeholder="Enter Username" name="username" required></input>
+                <input type="text" className="rInput" value={username} onChange={(e) => { setUsername(e.target.value) }} placeholder="Enter Username" name="username" required></input>
                 <label>password</label>
-                <input placeholder="Enter Password" type="password " name="password " required></input>
-                <button>Register</button>
+                <input placeholder="Enter Password" className="rInput" type="password " name="password " required></input>
+                <button onClick={handleClick}>Register</button>
             </form>
         </div>
     )
