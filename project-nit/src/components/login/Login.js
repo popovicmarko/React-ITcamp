3c4eff8e193649f3e208fd37109b6cd6596c8dc0
popovicmarko <popovicmarko350@gmail.com>
2023-05-05 17:21:33 +0200

    conecction backend and frontend (register and login)

--- project-nit/src/components/login/Login.js
+++ project-nit/src/components/login/Login.js
@@ -1,21 +1,37 @@
 import { useState } from "react";
 import './Login.css';
+import axios from 'axios';
 export function Login() {
-    function handleClick(e) {
 
+    async function Login(data) {
+        try {
+            const user = await axios.post('https://nit-backend.onrender.com/users/login', data)
+            const userInfo = await user.data
+            console.log(userInfo)
+        } catch (err) {
+            console.log(err)
+        }
     }
-    const [username, setUsername] = useState('')
+
+    function handleClick(e) {
+        e.preventDefault();
+        Login({ 
+            email, 
+            password})
+    }
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
     return (
         <div className="cointener">
             <form>
                 <h1>Login</h1>
-                <label className="label">Username</label>
-                <input className="input" type="text" value={username} onChange={(e) => { setUsername(e.target.value) }} placeholder="Username" name="username" required></input>
+                <label className="label">Email</label>
+                <input className="input" type="text" value={email} onChange={(e) => { setEmail(e.target.value) }} placeholder="Email" name="username" required></input>
                 <label className="label">Password</label>
-                <input className="input" type="password " name="password " placeholder="Passwors"  required></input>
+                <input className="input" value={password} onChange={(e) => { setPassword(e.target.value) }} type="password " name="password " placeholder="Passwors" required></input>
                 <button id="login" onClick={handleClick}>Login</button>
-                <p style={{ fontSize: '2rem', color: 'black' }}> {username}</p>
+                {/* <p style={{ fontSize: '2rem', color: 'black' }}> {email}</p> */}
             </form>
         </div>
     )
--- project-nit/src/components/register/Register.js
+++ project-nit/src/components/register/Register.js
@@ -1,22 +1,37 @@
 import { useState } from "react";
 import './Register.css'
+import axios from 'axios'
 export function Register() {
-    function handleClick(e) {
 
+        async function Register(data) {
+            try {
+                const user = await axios.post('https://nit-backend.onrender.com/users', data)
+                const userInfo = await user.data
+                console.log(userInfo)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    function handleClick(e) {
+        e.priventDefault();
+        Register({ 
+            name,
+            email, 
+            password})
     }
-    const [username, setUsername] = useState('')
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <div className='rCointener'>
             <form>
                 <h1>Register</h1>
                 <label>Name</label>
-                <input className="rInput" type="text" placeholder="First Name" name="firstname" required></input>
-                <label>Surame</label>
-                <input  type="text" className="rInput" placeholder="Last Name" name="lasttname" required></input>
-                <label>Username</label>
-                <input type="text" className="rInput" value={username} onChange={(e) => { setUsername(e.target.value) }} placeholder="Enter Username" name="username" required></input>
-                <label>password</label>
-                <input placeholder="Enter Password" className="rInput" type="password " name="password " required></input>
+                <input className="rInput" type="text" value={name} onChange={(e) => { setName(e.target.value) }} placeholder="Name" name="name" required></input>
+                <label>E-mail</label>
+                <input type="text" className="rInput" value={email} onChange={(e) => { setEmail(e.target.value) }} placeholder="Enter E-mail" name="email" required></input>
+                <label>Password</label>
+                <input placeholder="Enter Password" value={password} onChange={(e) => { setPassword(e.target.value) }} className="rInput" type="password "  name="password " required></input>
                 <button onClick={handleClick}>Register</button>
             </form>
         </div>
